b2c1a350a129ec5bca9b39a15a9ca85691a6dd55
TogoFire <italomellopereira@gmail.com>
2020-10-17 14:33:30 -0300

    defconfig and script: Drop hell name shit
    This reverts commit 0a3149fd48bd74b4824d8832fdc30e5d9a6e8311.

--- compile.sh
+++ compile.sh
@@ -151,7 +151,7 @@ Commit: \`$COMMIT_POINT\`
 
 Changelog [Here!]($CHANGELOG)
 
-Kernel: \`sleepy ~ but Paimon is emergency food\`"
+Kernel: \`Sleepy\`"
 }
 
 if [ "$(whoami)" = "lacia" ] || [ "$(whoami)" = "lacia-chan" ]; then
